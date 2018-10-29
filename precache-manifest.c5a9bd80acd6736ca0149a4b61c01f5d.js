30634c5a79cd1a8c090f76c244d450b27e117a66
linxu.dou <linxu.dou@vcg.com>
2018-10-29 18:10:37 +0800

    first commit

---
+++ precache-manifest.c5a9bd80acd6736ca0149a4b61c01f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pubg/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b4c25bb80e14b1873a23",
+    "url": "/pubg/static/js/runtime~main.b4c25bb8.js"
+  },
+  {
+    "revision": "c10376bc299a925d6f7c",
+    "url": "/pubg/static/js/main.c10376bc.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/pubg/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "c10376bc299a925d6f7c",
+    "url": "/pubg/static/css/main.058d66bd.chunk.css"
+  },
+  {
+    "revision": "840ae653bd80f669cf0219182f1bcbcb",
+    "url": "/pubg/index.html"
+  }
+];
